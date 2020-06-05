6b0e0438bed2f0403de77a34a20febef96af63a2
Shamil Samusenko <Shamilllaa@mail.ru>
2020-06-05 15:19:33 +0700

    3 commit

--- script/script.js
+++ script/script.js
@@ -11,7 +11,8 @@ for (let i = 0; i < numArr.length; i++) {
 console.log(mult); //выводим в консоль 
 
 // 3
-let multPow = [mult ** 3]; // Оператор ** используется  в ECMAScript 7 для возведения в степень
+let multPow = mult ** 3; // Оператор ** используется  в ECMAScript 7 для возведения в степень
+console.log('multPow: ', multPow);
 
 // 4
-console.log(multPow);
+console.log(+String(multPow).slice(0, 2)); //преобразуем число в строку, берем первые 2 символа и преобразуем их в число

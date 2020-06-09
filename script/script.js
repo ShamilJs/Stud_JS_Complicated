4b825e3799991114aeb32b715327960dd28afdd1
Shamil Samusenko <Shamilllaa@mail.ru>
2020-06-09 19:43:07 +0700

    lesson 4

--- script/script.js
+++ script/script.js
@@ -1,80 +1,29 @@
 'use strict';
 
+let userMessage = prompt('Введите сообщение', '');
+let step = 0;
 
-let lang = prompt('Введите язык (ru или en)', '');
+const testFunction = function (userData) {
+    userData = userData.split(''); //строку --> в массив и перебираем
+    userData.forEach(element => {
+        if (!!(+element)) {
+            console.log('Вы ввели не строку, пожалуйста повторите ввод');
+            step++;
+            return;
+        }
+    });
+    if (step !== 0) {
+        return;
+    } else {
+        userData = userData.join(''); //возвращаемся к строке
+        let a = userMessage.split(' ').join(''); //боремся с пробелами
+        console.log('Сообщение без пробелов: ', a);
 
-// 1
-// Через if
-let weekDayRu = [
-    'понедельник',
-    'вторник',
-    'среда',
-    'четверг',
-    'пятница',
-    'суббота',
-    'воскресенье',
-];
-let weekDayEn = [
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday',
-    'Sunday',
-];
-
-if (lang === 'ru') {
-
-    console.log('Выбрана неделя на русском: ', weekDayRu);
-
-} else if (lang === 'en') {
-    console.log('Выбрана неделя на английском: ', weekDayEn);
-}
-// Через switch
-switch (lang) {
-    case 'ru':
-        console.log('Выбрана неделя на русском: ', weekDayRu);
-        break;
-    case 'en':
-        console.log('Выбрана неделя на английском: ', weekDayEn);
-        break;
-}
-
-// через массив (применяю фильтрацию)
-let weekDay = [{
-        id: 'ru',
-        week: [
-            'понедельник',
-            'вторник',
-            'среда',
-            'четверг',
-            'пятница',
-            'суббота',
-            'воскресенье',
-        ]
-    },
-    {
-        id: 'en',
-        week: [
-            'Monday',
-            'Tuesday',
-            'Wednesday',
-            'Thursday',
-            'Friday',
-            'Saturday',
-            'Sunday',
-        ]
+        if (a.length > 30) {
+            let b = a.split('');
+            b.splice(30, (a.length - 30), '...');
+            console.log('Только 30 символов: ', b);
+        }
     }
-];
-
-let user = weekDay.find(item => item.id === `${lang}`);
-console.log('Выбранная неделя: ', user.week);
-
-// 2
-let namePerson = prompt('Введите имя', 'Артем');
-
-namePerson = (namePerson === 'Артем') ? 'директор' :
-    (namePerson === 'Максим') ? 'преподаватель' :
-    'студент';
-console.log('namePerson: ', namePerson);
+};
+testFunction(userMessage);

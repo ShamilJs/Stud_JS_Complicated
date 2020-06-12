9e2a4440f72d671ade7b7bbc9ac70e67a05342c6
Shamil Samusenko <Shamilllaa@mail.ru>
2020-06-13 03:55:00 +0700

    lesson 7

--- script/script.js
+++ script/script.js
@@ -1,36 +1,37 @@
 'use strict';
 
-// 1
+const element = document.querySelector('.new');
 
-let arr = [
-	'66462132326',
-	'465626123655',
-	'26465323565',
-	'6565945132445',
-	'88989898799',
-	'99999999977',
-	'24562187933',
-];
+let appWeekData = {
+    week: ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Восскресенье'],
+    getWeekConsole: function () {
+        console.log('Все дни недели: ', appWeekData.week);
+    },
+    getWeekParagraph: function () {
+        appWeekData.week.forEach((item) => {
+            console.log(item);
+        });
+    },
+    getWeekItalics: function () {
+        const newElement = document.createElement('p');
+        let newArr = appWeekData.week.slice(5);
+        newElement.innerHTML = `<p class="new"> <em> ${newArr}</em></p>
+        `;
+        element.append(newElement);
+        console.log(`%c${newArr}`, 'font-style: italic');
+    },
+    getWeekBold: function () {
+        let now = new Date();
+        let weekDay = appWeekData.week[now.getDay() - 1];
+        const newElement = document.createElement('p');
+        newElement.innerHTML = `<p class="new"> <b> ${weekDay}</b></p>
+        `;
+        element.append(newElement);
+        console.log(`%c${weekDay}`, 'font-weight: bold');
 
-const checkArr = function () {
-	for (let i = 0; i < arr.length; i++) {
-		if (arr[i].startsWith('2') || arr[i].startsWith('4')) {
-			console.log(`Искомое число ${i}:  `, +arr[i]);
-		}
-	}
+    }
 };
-checkArr();
-
-// 2
-
-const primeNumber = function (n) {
-	nextIteration: for (let i = 2; i <= n; i++) {
-		for (let j = 2; j < i; j++) {
-			if (i % j === 0) {
-				continue nextIteration;
-			}
-		}
-		console.log(i, `Делители этого числа: 1 и ${i}`);
-	}
-};
-primeNumber(100);
+appWeekData.getWeekConsole();
+appWeekData.getWeekParagraph();
+appWeekData.getWeekItalics();
+appWeekData.getWeekBold();

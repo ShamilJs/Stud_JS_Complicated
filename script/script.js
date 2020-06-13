8fea73fb60937d0a1d5a21bfe4f7c0dfea990033
Shamil Samusenko <Shamilllaa@mail.ru>
2020-06-14 03:56:16 +0700

    lesson 07

--- script/script.js
+++ script/script.js
@@ -3,33 +3,46 @@
 const element = document.querySelector('.new');
 
 let appWeekData = {
-    week: ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Восскресенье'],
-    getWeekConsole: function () {
-        console.log('Все дни недели: ', appWeekData.week);
-    },
-    getWeekParagraph: function () {
-        appWeekData.week.forEach((item) => {
-            console.log(item);
-        });
-    },
-    getWeekItalics: function () {
-        const newElement = document.createElement('p');
-        let newArr = appWeekData.week.slice(5);
-        newElement.innerHTML = `<p class="new"> <em> ${newArr}</em></p>
+	week: [
+		'Понедельник',
+		'Вторник',
+		'Среда',
+		'Четверг',
+		'Пятница',
+		'Суббота',
+		'Восскресенье',
+	],
+	getWeekConsole: function () {
+		console.log('Все дни недели: ', appWeekData.week);
+	},
+	getWeekParagraph: function () {
+		appWeekData.week.forEach((item) => {
+			console.log(item);
+		});
+	},
+	getWeekItalics: function () {
+		const newElement = document.createElement('p');
+		let week = appWeekData.week.slice(0, 5);
+		let newArr = appWeekData.week.slice(5);
+		newElement.innerHTML = `<p class="new">  ${week},<em>${newArr}</em></p>
         `;
-        element.append(newElement);
-        console.log(`%c${newArr}`, 'font-style: italic');
-    },
-    getWeekBold: function () {
-        let now = new Date();
-        let weekDay = appWeekData.week[now.getDay() - 1];
-        const newElement = document.createElement('p');
-        newElement.innerHTML = `<p class="new"> <b> ${weekDay}</b></p>
-        `;
-        element.append(newElement);
-        console.log(`%c${weekDay}`, 'font-weight: bold');
+		element.append(newElement);
+		console.log(`${week},%c${newArr}`, 'font-style: italic');
+	},
 
-    }
+	getWeekBold: function () {
+		let now = new Date();
+
+		let weekDay = appWeekData.week[now.getDay() + 6];
+
+		appWeekData.week.splice(now.getDay() + 6);
+
+		const newElement = document.createElement('p');
+		newElement.innerHTML = `<p class="new"> ${appWeekData.week}, <b>${weekDay}</b></p>
+		`;
+		element.append(newElement);
+		console.log(`${appWeekData.week}, %c${weekDay}`, 'font-weight: bold');
+	},
 };
 appWeekData.getWeekConsole();
 appWeekData.getWeekParagraph();

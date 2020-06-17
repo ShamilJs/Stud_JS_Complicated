4abcb44a123d5cc91de0ee7c06715ddb904b96b0
Shamil Samusenko <Shamilllaa@mail.ru>
2020-06-17 09:22:49 +0700

    lesson 9 rer

--- script/script.js
+++ script/script.js
@@ -1,9 +1,11 @@
 'use strict';
 
-const element = document.querySelector('.new');
+const element = document.querySelector('.new'),
+	new1 = document.querySelector('.new-1'),
+	new2 = document.querySelector('.new-2');
 
 let appDate = {
-	week: [
+	weekArray: [
 		'Восскресенье',
 		'Понедельник',
 		'Вторник',
@@ -12,7 +14,8 @@ let appDate = {
 		'Пятница',
 		'Суббота',
 	],
-	Month: [
+
+	MonthArray: [
 		'января',
 		'февраля',
 		'марта',
@@ -31,13 +34,11 @@ let appDate = {
 		let now = new Date();
 
 		appDate.date = now.getDate();
-
-		appDate.week = appDate.week[now.getDay()];
+		appDate.week = appDate.weekArray[now.getDay()];
 		appDate.indexWeek = now.getDay();
-		appDate.Month = appDate.Month[now.getMonth()];
+		appDate.Month = appDate.MonthArray[now.getMonth()];
 		appDate.indexMonth = now.getMonth() + 1;
 		appDate.year = now.getFullYear();
-
 		appDate.hours = now.getHours();
 		appDate.minutes = now.getMinutes();
 		appDate.seconds = now.getSeconds();
@@ -70,6 +71,7 @@ let appDate = {
 			}
 		}
 	},
+
 	getDeclensionOfHours: function () {
 		if (appDate.hours === 1 || appDate.hours === 21) {
 			return 'час';
@@ -86,6 +88,7 @@ let appDate = {
 			return 'часов';
 		}
 	},
+
 	getDatePlusNull: function () {
 		appDate.date += '';
 		appDate.indexMonth += '';
@@ -113,31 +116,32 @@ let appDate = {
 			appDate.seconds = '0' + appDate.seconds;
 		}
 	},
+
+	getCurrenTimeString: function (argument) {
+		appDate.getDate();
+		appDate.getDatePlusNull();
+
+		argument.innerHTML = `<p class="new1"> <b>${appDate.date}.${appDate.indexMonth}.${appDate.year} 
+        - ${appDate.hours}:${appDate.minutes}:${appDate.seconds} </b></p>
+        `;
+	},
+
 	getDataFormatA: function () {
 		appDate.getDate();
 
-		const newElement = document.createElement('p');
-
-		newElement.innerHTML = `<p class="new"> <b> Сегодня ${appDate.week},
+		element.innerHTML = `<p class="new"> <b> Сегодня ${appDate.week},
         ${appDate.date} ${appDate.Month} ${appDate.year} года,
         ${appDate.hours} ${appDate.getDeclensionOfHours()} ${
 			appDate.minutes
 		} ${appDate.getMinutesOrSecond(appDate.minutes)} ${appDate.seconds}
         ${appDate.getMinutesOrSecond(appDate.seconds)}</b></p>
 	    `;
-		element.append(newElement);
 	},
 	getDataFormatB: function () {
-		appDate.getDatePlusNull();
-
-		const newElement = document.createElement('p');
-		newElement.innerHTML = `<p class="new"> <b>${appDate.date}.${appDate.indexMonth}.${appDate.year} 
-        - ${appDate.hours}:${appDate.minutes}:${appDate.seconds} </b></p>
-	    `;
-		element.append(newElement);
+		appDate.getCurrenTimeString(new1);
 	},
 	timerDate: function () {
-		let timer = setInterval(() => appDate.getDataFormatB(), 1000);
+		setInterval(() => appDate.getCurrenTimeString(new2), 1000);
 	},
 };
 appDate.getDataFormatA();

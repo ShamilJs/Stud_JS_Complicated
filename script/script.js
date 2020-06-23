210505a2bf1c5f0c0b78bbc817f4d6e40c259897
ShamilJs <66412234+ShamilJs@users.noreply.github.com>
2020-06-23 22:46:34 +0700

    lesson13rer

--- script/script.js
+++ script/script.js
@@ -8,8 +8,12 @@ button.addEventListener('click', function(){
 	let r = Math.floor(Math.random() * (256)),
    		g = Math.floor(Math.random() * (256)),
    		b = Math.floor(Math.random() * (256)),
+         q = Math.floor(Math.random() * (10)),
    		color = '#' + r.toString(16) + g.toString(16) + b.toString(16);
-   		console.log(color);
+       		if (color.length < 7) {
+   			color += q.toString(16);
+   		}
+   		
    		codeColor.textContent = color;
    		document.body.style.background = color;
 
